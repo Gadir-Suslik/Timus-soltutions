6593c657ad6e02f51f7eb695bb3cb4a4bfc3a76c
Gadir-Suslik <127016828+Gadir-Suslik@users.noreply.github.com>
2025-02-06 22:31:51 +0400

    Create 1293.cpp

---
+++ 1293.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fs first
+#define se second
+#define all(v) v.begin(),v.end()
+
+int main() {
+  int n,a,b;
+  cin>>n>>a>>b;
+  cout<<n*a*b*2<<'\n';
+}
