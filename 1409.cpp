e92149e6095fbee9b4a1f690e07ecd7470f3d27b
Gadir-Suslik <127016828+Gadir-Suslik@users.noreply.github.com>
2025-02-06 22:36:15 +0400

    Create 1409.cpp

---
+++ 1409.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fs first
+#define se second
+#define all(v) v.begin(),v.end()
+
+int main() {
+  int n,m;
+  cin>>n>>m;
+  int k=n+m-1;
+  cout<<k-n<<" "<<k-m<<'\n';
+}
