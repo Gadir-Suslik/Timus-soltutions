dd6bb950fe3a3448a2799391bf875afaa37274f2
Gadir-Suslik <127016828+Gadir-Suslik@users.noreply.github.com>
2025-02-06 22:46:32 +0400

    Create 2012.cpp

---
+++ 2012.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fs first
+#define se second
+#define all(v) v.begin(),v.end()
+
+int main() {
+  int n;
+  cin>>n;
+  int k=45*(12-n);
+  cout<<((k>240) ? "NO" : "YES");
+}
