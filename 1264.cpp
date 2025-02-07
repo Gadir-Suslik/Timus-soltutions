492b6bf87d4a67a6706f3be791340e58ac5c95b1
Gadir-Suslik <127016828+Gadir-Suslik@users.noreply.github.com>
2025-02-07 12:40:58 +0400

    Create 1264.cpp

---
+++ 1264.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fs first
+#define se second
+#define all(v) v.begin(),v.end()
+
+int main() {
+  int n,m;
+  cin>>n>>m;
+  cout<<n*(++m)<<'\n';
+}
